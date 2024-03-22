69640307f8b7533aef3614eb6cc2b798a38287f8
geraldfivetrg <153238428+geraldfivetrg@users.noreply.github.com>
2024-03-23 06:12:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,34 @@ document.addEventListener('click', function (e) {
     window.open('https://www.fivetalentsrealty.com/va-loan-pre-qualification', '_blank');
   }
 });
+
+// Function to highlight the state on the map when selected from the dropdown
+function highlightState(stateId) {
+  var paths = document.getElementsByTagName('path');
+  for (var i = 0; i < paths.length; i++) {
+    if (paths[i].id === stateId) {
+      paths[i].classList.add('highlighted');
+    } else {
+      paths[i].classList.remove('highlighted');
+    }
+  }
+}
+
+// Add an onchange event to the dropdown
+document.getElementById('stateDropdown').addEventListener('change', function(e) {
+  highlightState(e.target.value);
+});
+
+// Modify the existing mouseover event to also update the dropdown
+document.addEventListener('mouseover', function (e) {
+  if (e.target.tagName === 'path') {
+    var stateId = e.target.id;
+    document.getElementById('stateDropdown').value = stateId;
+    var stateName = e.target.dataset.name;
+    detailsBox.innerHTML = stateName;
+    detailsBox.style.opacity = "100%";
+  } else {
+    detailsBox.style.opacity = "0%";
+  }
+});
+
